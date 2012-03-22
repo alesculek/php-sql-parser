18d2805618c6abd6104f3f9573302f3334e48532
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2012-03-22 11:43:52 +0000

    The "whitespace" branch has reached a release state. This is a savepoint just before merging into trunk.

---
+++ tags/2012-03-22-whitespace/testsuite/tests-creator.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * execute all tests
+ */
+require_once(dirname(__FILE__) . '/tests/creator/asc.php');
+require_once(dirname(__FILE__) . '/tests/creator/function.php');
+require_once(dirname(__FILE__) . '/tests/creator/inlist.php');
+require_once(dirname(__FILE__) . '/tests/creator/insert.php');
+require_once(dirname(__FILE__) . '/tests/creator/join.php');
+require_once(dirname(__FILE__) . '/tests/creator/left.php');
+require_once(dirname(__FILE__) . '/tests/creator/tableexpr.php');
+require_once(dirname(__FILE__) . '/tests/creator/where.php');
---
+++ tags/2012-03-22-whitespace/testsuite/tests-parser.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * execute all tests
+ */
+require_once(dirname(__FILE__) . '/tests/parser/aliases.php');
+require_once(dirname(__FILE__) . '/tests/parser/allcolumns.php');
+require_once(dirname(__FILE__) . '/tests/parser/backtick.php');
+require_once(dirname(__FILE__) . '/tests/parser/delete.php');
+require_once(dirname(__FILE__) . '/tests/parser/from.php');
+require_once(dirname(__FILE__) . '/tests/parser/gtltop.php');
+require_once(dirname(__FILE__) . '/tests/parser/inlist.php');
+require_once(dirname(__FILE__) . '/tests/parser/insert.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue11.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue12.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue15.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue21.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue25.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue30.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue31.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue32.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue34.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue36.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue37.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue38.php');
+require_once(dirname(__FILE__) . '/tests/parser/left.php');
+require_once(dirname(__FILE__) . '/tests/parser/nested.php');
+require_once(dirname(__FILE__) . '/tests/parser/positions.php');
+require_once(dirname(__FILE__) . '/tests/parser/select.php');
+require_once(dirname(__FILE__) . '/tests/parser/subselect.php');
+require_once(dirname(__FILE__) . '/tests/parser/union.php');
+require_once(dirname(__FILE__) . '/tests/parser/update.php');
+require_once(dirname(__FILE__) . '/tests/parser/zero.php');
+
+exit;
