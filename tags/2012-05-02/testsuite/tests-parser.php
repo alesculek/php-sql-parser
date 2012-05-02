c29639e8027f06171ef42eda7bbab8de280b0992
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2012-05-02 12:45:35 +0000

    FIX: issue 41 FIX: issue 45 CHG: class structure, some more OOP

---
+++ tags/2012-05-02/testsuite/tests-parser.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * execute all tests
+ */
+require_once(dirname(__FILE__) . '/tests/parser/aliases.php');
+require_once(dirname(__FILE__) . '/tests/parser/allcolumns.php');
+require_once(dirname(__FILE__) . '/tests/parser/backtick.php');
+require_once(dirname(__FILE__) . '/tests/parser/delete.php');
+require_once(dirname(__FILE__) . '/tests/parser/from.php');
+require_once(dirname(__FILE__) . '/tests/parser/gtltop.php');
+require_once(dirname(__FILE__) . '/tests/parser/inlist.php');
+require_once(dirname(__FILE__) . '/tests/parser/insert.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue11.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue12.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue15.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue21.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue25.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue30.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue31.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue32.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue34.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue36.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue37.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue38.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue39.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue40.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue41.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue42.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue43.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue44.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue45.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue46.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue51.php');
+require_once(dirname(__FILE__) . '/tests/parser/left.php');
+require_once(dirname(__FILE__) . '/tests/parser/nested.php');
+require_once(dirname(__FILE__) . '/tests/parser/positions.php');
+require_once(dirname(__FILE__) . '/tests/parser/select.php');
+require_once(dirname(__FILE__) . '/tests/parser/subselect.php');
+require_once(dirname(__FILE__) . '/tests/parser/union.php');
+require_once(dirname(__FILE__) . '/tests/parser/update.php');
+require_once(dirname(__FILE__) . '/tests/parser/zero.php');
