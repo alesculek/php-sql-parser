327c52265a0caf2c3a6b79c2954ae916c02ed278
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2012-05-07 09:51:32 +0000

    FIX: issue 53 FIX: issue 54 CHG: the output has been changed for LIMIT clauses!

---
+++ tags/2012-05-07/testsuite/tests-creator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * execute all tests
+ */
+require_once(dirname(__FILE__) . '/tests/creator/asc.php');
+require_once(dirname(__FILE__) . '/tests/creator/count_distinct.php');
+require_once(dirname(__FILE__) . '/tests/creator/function.php');
+require_once(dirname(__FILE__) . '/tests/creator/inlist.php');
+require_once(dirname(__FILE__) . '/tests/creator/insert.php');
+require_once(dirname(__FILE__) . '/tests/creator/join.php');
+require_once(dirname(__FILE__) . '/tests/creator/left.php');
+require_once(dirname(__FILE__) . '/tests/creator/tableexpr.php');
+require_once(dirname(__FILE__) . '/tests/creator/update.php');
+require_once(dirname(__FILE__) . '/tests/creator/where.php');
