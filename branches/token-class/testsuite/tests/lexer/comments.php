774753ad89816447338b0f21c5b1085167346dad
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2012-08-24 07:18:48 +0000

    ADD: some lexer tests for comments

--- branches/token-class/testsuite/tests/lexer/comments.php
+++ branches/token-class/testsuite/tests/lexer/comments.php
@@ -1,1 +1,22 @@
 <?php
+require_once(dirname(__FILE__) . "/../../../classes/lexer.php");
+require_once(dirname(__FILE__) . "/../../test-more.php");
+
+$lexer = new PHPSQLLexer();
+
+$sql = "SELECT --comment
+* 
+FROM 
+-- comment
+table";
+$s = $lexer->split($sql);
+print_r($s);
+$expected = getExpectedValue(dirname(__FILE__), 'parenthesis.serialized');
+eq_array($s, $expected, 'some inline comments');
+
+
+$sql = "insert /* +APPEND */ into TableName (Col1,col2) values(1,'pol')";
+$s = $lexer->split($sql);
+print_r($s);
+$expected = getExpectedValue(dirname(__FILE__), 'parenthesis.serialized');
+eq_array($s, $expected, 'a multiline comment');
